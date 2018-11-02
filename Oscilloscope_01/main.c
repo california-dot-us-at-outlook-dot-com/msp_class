11aa1a3d3f58f9a38a2064d04fb4d98dd705ca2c
cal <california.us@outlook.com>
2018-11-02 21:58:25 +0800

    添加波形居中功能

--- Oscilloscope_01/main.c
+++ Oscilloscope_01/main.c
@@ -13,21 +13,27 @@ unsigned char *wave11;
 unsigned long timeDiv;
 long scale=1000;
 unsigned char pause=0;
+unsigned char middle=0;//使波形居中
+#define bia -10//bia最小为-10
 
 #pragma vector = PORT1_VECTOR
 __interrupt void Port1_ISR(void){
-    if(!scale){
-        scale=1000;
-    }
-    P6OUT=~P6OUT;
+
     if(!(P1IN&BIT0)){
         scale>>=1;
     }else if(!(P1IN&BIT1)){
         scale<<=1;
     }else if(!(P1IN&BIT2)){
         pause=!pause;
+    }else if(!(P1IN&BIT3)){
+        middle=!middle;
     }
-
+    if(!scale){
+        scale=1000;
+    }
+    if(scale>65536){
+        scale=1000;
+    }
     P1IFG &= 0;
 }
 
@@ -50,8 +56,8 @@ int main(void)
 	        P6DIR=0xff;
 	        P1DIR &= 0;
 	        P1SEL=0x00;
-	            P1IES |= BIT0+BIT1+BIT2; //0:上升沿；    1：下降沿
-	            P1IE |= BIT0+BIT1+BIT2;  //中断允许，1为允许
+	            P1IES |= BIT0+BIT1+BIT2+BIT3; //0:上升沿；    1：下降沿
+	            P1IE |= BIT0+BIT1+BIT2+BIT3;  //中断允许，1为允许
 	            P1IFG &= 0;  //中断标志，0为可接受中断
 	            _EINT();    //开启总中断
 
@@ -72,11 +78,11 @@ int main(void)
 	        Write_Data(0,0x00);
 	    }
 	}
-	LCD_SetPos(0,239,285,285);
+	LCD_SetPos(0,239,275+bia,275+bia);
 	    for(lx=0;lx<240;lx++){
 	        Write_Data(0x00,0x1f);
 	}
-	LCD_SetPos(0,239,20,20);
+	LCD_SetPos(0,239,10+bia,10+bia);
 	    for(lx=0;lx<240;lx++){
 	        Write_Data(0xff,0xff);
 	    }
@@ -87,19 +93,32 @@ int main(void)
 	                            Write_Data(0xf8,0x1f);
 	                    }
 	unsigned long loop=0;
+	unsigned char waveTop=0;
+	unsigned char waveBottom=0;
+	unsigned char waveMax=0;
+	unsigned char waveMin=0;
 	while(1){
+	    int bias=128-((waveTop-waveBottom)/2);
 	    //采样
 	    /////////////////////////////////////////////////////////////
+
 	    for(loop=0;loop<61;loop++){
 	        ly=(adcTrans());
+	        if(middle){
+	            wave1[loop]=(ly>>4)+bias;
+	        }else{
+	            wave1[loop]=ly>>4;
+	            waveMax=(wave1[loop]>waveMax?wave1[loop]:waveMax);
+	            waveMin=(wave1[loop]<waveMin?wave1[loop]:waveMin);
+	        }
 
-	        wave1[loop]=ly>>4;
 	        //wave11[i/2]=ly>>4;
 	        for(lx=0;lx<scale;lx++){
 	            delay_us(1);
 	        }
 	    }
-
+	    waveTop=waveMax;
+	    waveBottom=waveMin;
 	    ///////////////////////////////////////////////////////////////
 
 //画曲线
@@ -117,7 +136,7 @@ int main(void)
 	            }
                 ramp=ramp>4?ramp:4;
 
-	                height=280-wave1[lx/4];
+	                height=bia+270-wave1[lx/4];
 
 	            if(upordown){
 	                LCD_SetPos(lx,lx+4,height,height+ramp);
@@ -148,7 +167,7 @@ int main(void)
 	            }
 	            ramp=ramp>4?ramp:4;
 
-	                                            height=280-wave1[lx/4];
+	                                            height=bia+270-wave1[lx/4];
 
 	                        if(upordown){
 	                                            LCD_SetPos(lx,lx+4,height,height+ramp);
@@ -166,7 +185,7 @@ int main(void)
 	        /////////////////////////////////////////////////////////////////////////////
 	        //画1/2、1/4、3/4线
 	                //////////////////////////////////////////////////////////////////
-	                        LCD_SetPos(0,239,153,153);
+	                        LCD_SetPos(0,239,143+bia,143+bia);
 	                        for(lx=0;lx<240;lx++){
 	                            if(lx%3){
 	                                Write_Data(0x07,0xe0);
@@ -174,7 +193,7 @@ int main(void)
 	                            Write_Data(0x00,0x00);
 	                        }
 	                    }
-	                        LCD_SetPos(0,239,86,86);
+	                        LCD_SetPos(0,239,76+bia,76+bia);
 	                                        for(lx=0;lx<240;lx++){
 	                                            if(!(lx%3)){
 	                                                Write_Data(0xf8,0x1f);
@@ -182,10 +201,10 @@ int main(void)
 	                                            Write_Data(0x00,0x00);
 	                                        }
 	                                    }
-	                                        LCD_SetPos(0,239,219,219);
+	                                        LCD_SetPos(0,239,209+bia,209+bia);
 	                                                        for(lx=0;lx<240;lx++){
 	                                                            if(!(lx%3)){
-	                                                                Write_Data(0xff,0xe0);
+	                                                                Write_Data(0xf0,0xe0);
 	                                                            }else{
 	                                                            Write_Data(0x00,0x00);
 	                                                        }
